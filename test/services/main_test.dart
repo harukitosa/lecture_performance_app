3701c116af0596bba92a941da81612aa0d5fdc06
土佐陽生 <13haruki28@gmail.com>
2020-03-12 16:30:46 +0900

    DEVELOP:STUDENTサービスの作成

--- test/services/main_test.dart
+++ test/services/main_test.dart
@@ -1,6 +1,8 @@
 import 'HomeRoom_test.dart';
 import 'Semester_test.dart';
+import 'Student_test.dart';
 void main() {
   testHomeRoomService();
   testsemesterService();
+  teststudentService();
 }
