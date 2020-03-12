d0bc769e7dbcf233552160fb8f8bfd19c94e066d
土佐陽生 <13haruki28@gmail.com>
2020-03-12 16:51:16 +0900

    DEVELOP:評価タイプサービスの作成

--- test/services/main_test.dart
+++ test/services/main_test.dart
@@ -1,8 +1,10 @@
 import 'HomeRoom_test.dart';
 import 'Semester_test.dart';
 import 'Student_test.dart';
+import 'EvaluationType.dart';
 void main() {
   testHomeRoomService();
   testsemesterService();
   teststudentService();
+  testEvaluationTypeService();
 }
