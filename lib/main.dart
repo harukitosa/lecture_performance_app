8c5610a296ac785956a16bb00c757e5b4fa59a52
土佐陽生 <13haruki28@gmail.com>
2020-06-17 14:10:19 +0900

    Fix:修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './routes/index.dart';
 import 'package:flutter/rendering.dart';
+// flutter
 void main() {
   debugPaintSizeEnabled = false;
   runApp(App());
