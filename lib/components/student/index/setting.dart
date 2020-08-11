75b4e4abdb8192ee8ac2a70e4f86de60ea031a4d
harukitosa <13haruki28@gmail.com>
2020-08-11 10:32:26 +0900

    fix

--- lib/components/student/index/setting.dart
+++ lib/components/student/index/setting.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:lecture_performance_app/db/models/homeroom.dart';
-import 'package:lecture_performance_app/providers/homeroom_provider.dart';
+import 'package:lecture_performance_app/providers/student_index_provider.dart';
+import 'package:lecture_performance_app/wire.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 //routerで渡される値
@@ -16,27 +20,40 @@ class StudentSetting extends StatelessWidget {
   Widget build(BuildContext context) {
     final args =
         ModalRoute.of(context).settings.arguments as StudentSettingArgument;
+    final student = initStudentAPI();
+    final evaluation = initEvaluationAPI();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           '${args.homeRoom.grade}年${args.homeRoom.lectureClass}組',
         ),
       ),
-      body: Consumer<HomeRoomProvider>(
-        builder: (context, counter, _) {
-          return ListView.builder(
-            itemBuilder: (context, int index) {
-              return Center(
-                child: Container(
-                  width: 800,
-                  child: const Center(child: _ListCard('データのインポート')),
-                ),
-              );
-            },
-            itemCount: 1,
-          );
-          ;
-        },
+      body: MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(
+            value: StudentIndexProvider(
+              student: student,
+              evaluation: evaluation,
+              homeroomID: args.homeRoom.id,
+            ),
+          ),
+        ],
+        child: Consumer<StudentIndexProvider>(
+          builder: (context, counter, _) {
+            return ListView.builder(
+              itemBuilder: (context, int index) {
+                return Center(
+                  child: Container(
+                    width: 800,
+                    child: const Center(child: _ListCard('データのインポート')),
+                  ),
+                );
+              },
+              itemCount: 1,
+            );
+          },
+        ),
       ),
     );
   }
@@ -52,11 +69,8 @@ class _ListCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-//        outButton();
-//        getFilePath().then((value) {
-//          launch('$value');
-//          print(value);
-//        });
+        outButton();
+        getFilePath().then(print);
       },
       child: Card(
         child: Padding(
@@ -82,20 +96,22 @@ class _ListCard extends StatelessWidget {
   }
 }
 
-//void outButton() {
-//  getFilePath().then((File file) {
-//    file.writeAsString('helloworld');
-//  });
-//}
-//
-//Future<File> getFilePath() async {
-//  final directory = await getTemporaryDirectory();
-//  return File('${directory.path}/sample.txt');
-//}
+void outButton() {
+  getFilePath().then((File file) {
+    file.writeAsString('ありがとうございました');
+  });
+}
 
-//テキストファイルの読み込み
-//Future load() async {
-//  final file = await getFilePath();
-//  await file.writeAsBytes('helloworld');
-//  return file;
-//}
+//
+Future<File> getFilePath() async {
+  final directory = await getApplicationDocumentsDirectory();
+  if (directory == null) {
+    return null;
+  }
+  return File('${directory.path}/sample.txt');
+}
+
+Future load() async {
+  final file = await getFilePath();
+  return file;
+}
